6e01ce559ea1fba6e8d6f16f127f361d2ef2cf1c
BleedingChips <chips_code@outlook.com>
2024-01-22 01:14:46 +0800

    同步更新

--- Test/DumplingFormTest.cpp
+++ Test/DumplingFormTest.cpp
@@ -10,13 +10,16 @@ using namespace Dumpling;
 int main()
 {
 
-	auto style = Dumpling::Win32::FormStyle::CreateDefaultGameplayStyle();
-	auto form = Win32::Form::Create();
+	{
+		auto style = Dumpling::Win32::FormStyle::CreateDefaultGameplayStyle();
+		auto form = Win32::Form::Create();
 
-	Potato::Task::TaskContext context;
-	form->Commit(context, std::this_thread::get_id(), style, {});
+		Potato::Task::TaskContext context;
+		form->Commit(context, std::this_thread::get_id(), style, {});
 
-	context.ProcessTask({});
+		context.ProcessTaskUntillNoExitsTask({});
+	}
+	
 
 	return 0;
 }
